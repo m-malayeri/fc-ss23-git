11d8221c04eb64fbb2e9a6c5e236a176211778e4
Kevin Patel <47410011+kvnptl@users.noreply.github.com>
2023-03-15 00:55:50 +0100

    new: init calculator code

---
+++ calculator/calculator_application.py
@@ -0,0 +1,14 @@
+"""
+Author: Kevin Patel
+Date: 14.03.2023
+Email: ltpnvk@kmail.com
+"""
+
+def initilization():
+	print("Initiating Calculator ...")
+  
+def main():
+	initilization()
+
+def __name__ == "__main__":
+  main()
